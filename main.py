5041fa0febb4919acd333e6647400c5854e88b69
Lim Koon Kiat <lim_koon_kiat@cpf.gov.sg>
2025-07-29 11:09:34 +0800

    Add chat

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from logic.query_handler import process_user_message
 
 st.title('Main Page')
+
+form = st.form(key='form')
+form.subheader("Prompt")
+
+user_prompt = form.text_area("Enter your prompt here:", height=200)
+
+if form.form_submit_button("Submit"):
+    st.toast(f"User Input Submitted - {user_prompt}")
+    response = process_user_message(user_prompt)
+    st.write(response)
